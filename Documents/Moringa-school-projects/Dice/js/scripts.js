123b0d17e1c438faa300d1ef3edb7941dfc30e55
Charles maina <muragemaina09@gmail.com>
2019-04-05 12:37:49 +0300

    created player 2 function

--- Documents/Moringa-school-projects/Dice/js/scripts.js
+++ Documents/Moringa-school-projects/Dice/js/scripts.js
@@ -16,10 +16,30 @@ $(document).ready(function () {
             $("butoon#roll-dice2").show();
 
             $(".result1").html("player 1: 0 and total score is 0");
-        }else {
-            $(".result1").html("player 1 current scrore: " +player1.currentScore +" total score " +player1.totalScore );
+        } else {
+            $(".result1").html("player 1 current scrore: " + player1.currentScore + " total score " + player1.totalScore);
         }
 
+        if (player1.totalScore === 100) {
+            alert("Player 1 wins!!");
+        }
 
+        $("button#roll-dice2").on("click", function () {
+            Player.prototype.currentScore = Math.floor(Math.random() * 6) + 1;
+            player2.totalScore += player2.currentScore;
+
+            if (player2.currentScore === 1) {
+                alert("You have scored 1!! Back to player 1!");
+                $("button#roll-dice2").hide();
+                $("butoon#roll-dice1").show();
+                $(".result2").html("player 2: 0 and total score is 0");
+            } else {
+                $(".result2").html("player 2 current score : " + player2.currentScore + " total score" + player2.totalScore);
+            }
+
+            if (player2.totalScore === 100) {
+                alert("Player 2 wins!!");
+            }
+        })
     })
 })
