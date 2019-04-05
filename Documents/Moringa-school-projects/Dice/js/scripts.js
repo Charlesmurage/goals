a495e39b71c4cda02620b2f3898425e3c7c48f50
Charles maina <muragemaina09@gmail.com>
2019-04-05 12:19:34 +0300

    created post for player1 reults

--- Documents/Moringa-school-projects/Dice/js/scripts.js
+++ Documents/Moringa-school-projects/Dice/js/scripts.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
-    function Player(id,score,totalScore) {
-        this.id=id;
-        this.totalScore= 0;
+    function Player(id, score, totalScore) {
+        this.id = id;
+        this.totalScore = 0;
     }
-    player1= new Player();
-    player2= new Player();
+    player1 = new Player();
+    player2 = new Player();
 
-$("button#roll-dice1").on("click", function () {
-    Player.prototype.currentScore = Math.floor(Math.random() *6) +1;
-    player1.totalScore += player1.currentScore;
+    $("button#roll-dice1").on("click", function () {
+        Player.prototype.currentScore = Math.floor(Math.random() * 6) + 1;
+        player1.totalScore += player1.currentScore;
 
-    if (player1.currentScore===1) {
-        alert("You have scored 1! Next player!");
-        $("button#roll-dice1").hide();
-        $("butoon#roll-dice2").show();
+        if (player1.currentScore === 1) {
+            alert("You have scored 1! Next player!");
+            $("button#roll-dice1").hide();
+            $("butoon#roll-dice2").show();
 
-        $(".result1").html("player 1: 0 and total score is 0");
-    }
+            $(".result1").html("player 1: 0 and total score is 0");
+        }else {
+            $(".result1").html("player 1 current scrore: " +player1.currentScore +" total score " +player1.totalScore );
+        }
 
-})
+
+    })
 })
